ab02bda460d76313ccb98d50018a492fe41f1cc2
stojanJ <stojanjanjic91@gmail.com>
2022-08-03 21:26:59 +0200

    zadatak 2 i 3

---
+++ single-post.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Single-post</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="styles/blog.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <?php
+    include('header.php');
+    ?>
+</header>
+<main>
+<aside class="col-sm-3 ml-sm-auto blog-sidebar">
+           <?php
+            include('sidebar.php');
+           ?>
+        </aside>
+
+</main>
+<footer class="blog-footer">
+   <?php
+   include('footer.php');
+   ?>
+</footer>
+
+</body>
