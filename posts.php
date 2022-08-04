e3a45b724ee86074dd362db701068c2a6f2e1dc4
stojanJ <stojanjanjic91@gmail.com>
2022-08-04 08:22:59 +0200

    4 i 5 zadatak

--- posts.php
+++ posts.php
@@ -1,15 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "academy";
-$password = "academy";
+$username = "root";
+$password = "vivify";
 $databaseName = "blog";
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$databaseName", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
 } catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
 }
 
 ?>
@@ -43,74 +41,24 @@ try {
 
 <main role="main" class="container">
 <?php
-$sql = "SELECT * FROM posts ORDER BY created_at  ";
+$sql = "SELECT * FROM posts ORDER BY created_at DESC ";
 $statement = $conn->prepare($sql);
 $statement->execute();
 $statement->setFetchMode(PDO::FETCH_ASSOC);
 $posts = $statement->fetchAll();
-print_r($posts);
 ?>
     <div class="row">
 
         <div class="col-sm-8 blog-main">
 
             <div class="blog-post">
-                <h2 class="blog-post-title"><a class="link" href="#">Sample blog post</a></h2>
-                <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+                <?php foreach ($posts as $post){?>
 
-                <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-                <hr>
-                <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-                <blockquote>
-                    <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </blockquote>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <h2>Heading</h2>
-                <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                <h3>Sub-heading</h3>
-                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                <pre><code>Example code block</code></pre>
-                <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-                <h3>Sub-heading</h3>
-                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                <ul>
-                    <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                    <li>Donec id elit non mi porta gravida at eget metus.</li>
-                    <li>Nulla vitae elit libero, a pharetra augue.</li>
-                </ul>
-                <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-                <ol>
-                    <li>Vestibulum id ligula porta felis euismod semper.</li>
-                    <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
-                    <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-                </ol>
-                <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
-            </div><!-- /.blog-post -->
+                <h2 class="blog-post-title"><a class="link" href="single-post.php?post_id=<?php echo($post['id']) ?>"><?php echo($post['Title']) ?></a></h2>
+                <p class="blog-post-meta"><?php echo($post['Created_at']) ?> <a href="#"><?php echo($post['Author']) ?></a></p>
 
-            <div class="blog-post">
-                <h2 class="blog-post-title"><a class="link" href="#">Another blog post</a></h2>
-                <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
-
-                <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-                <blockquote>
-                    <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </blockquote>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-            </div><!-- /.blog-post -->
-
-            <div class="blog-post">
-                <h2 class="blog-post-title"><a class="link" href="#">New feature</a></h2>
-                <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
-
-                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                <ul>
-                    <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                    <li>Donec id elit non mi porta gravida at eget metus.</li>
-                    <li>Nulla vitae elit libero, a pharetra augue.</li>
-                </ul>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
+                <p><?php echo($post['Body']) ?></p>
+                <?php } ?>
             </div><!-- /.blog-post -->
 
             <nav class="blog-pagination">
--- single-post.php
+++ single-post.php
@@ -1,3 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "vivify";
+$databaseName = "blog";
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$databaseName", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -15,6 +28,7 @@
 
     <!-- Custom styles for this template -->
     <link href="styles/blog.css" rel="stylesheet">
+    <link href="styles/styles.css" rel="stylesheet">
 </head>
 <body>
 <header>
@@ -23,6 +37,60 @@
     ?>
 </header>
 <main>
+    <?php
+    if (isset($_GET['post_id'])) {
+        $sql = "SELECT * FROM posts WHERE id = {$_GET['post_id']}";
+                    $statement = $conn->prepare($sql);
+                    $statement->execute();
+                    $statement->setFetchMode(PDO::FETCH_ASSOC);
+                    $singlePost = $statement->fetch();
+                        //  print_r($singlePost);
+                        ?>
+    <div class="row">
+        <div class="col-sm-8 blog-main">
+
+            <div class="blog-post">
+                
+                <h2 class="blog-post-title"><a class="link" href="single-post.php?post_id=<?php echo($singlePost['id']) ?>"><?php echo($singlePost['Title']) ?></a></h2>
+                <p class="blog-post-meta"><?php echo($singlePost['Created_at']) ?> <a href="#"><?php echo($singlePost['Author']) ?></a></p>
+
+                <p><?php echo($singlePost['Body']) ?></p>
+            
+            </div><!-- /.blog-post -->
+            <?php
+            $sql = "SELECT * FROM comments WHERE comments.post_id = {$_GET['post_id']}";
+                    $statement = $conn->prepare($sql);
+
+                    // izvrsavamo upit
+                    $statement->execute();
+
+                    // zelimo da se rezultat vrati kao asocijativni niz.
+                    // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+                    $statement->setFetchMode(PDO::FETCH_ASSOC);
+
+                    // punimo promenjivu sa rezultatom upita
+                    $comments = $statement->fetchAll();
+                    ?>
+                    <h3>comments</h3>
+                    <?php foreach($comments as $comment) { ?>
+                            <ul>
+                                <li>posted by: <strong><?php echo $comment['Author'] ?></strong><br>
+                                <?php echo $comment['Text'] ?><hr>
+                                </li>
+                            </ul>
+                           <?php } ?>   
+            <nav class="blog-pagination">
+                <a class="btn btn-outline-primary" href="#">Older</a>
+                <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+            </nav>
+
+        </div><!-- /.blog-main -->
+            <?php
+                } else {
+                    echo('post_id nije prosledjen kroz $_GET');
+                    header("Location: ./posts.php");
+                }
+            ?>
 <aside class="col-sm-3 ml-sm-auto blog-sidebar">
            <?php
             include('sidebar.php');
